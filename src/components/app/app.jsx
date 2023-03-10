811ee48c7e8c6cbad177d80eead9436b669a6612
Bogdan Gordin <bogdan_g@inbox.ru>
2023-03-10 14:09:51 +0300

    feat: counter deleted

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,20 +3,11 @@ import { useState } from 'react';
 import City from '../../components/city/city';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
   return (
     <>
       <City city={"Москва"}/>
       <City city={"Пенза"}/>
       <City city={"Саратов"}/>
-      <div className="counter">
-        <button onClick={() => setCounter((prev) => prev + 1)} className="counter__button counter__plus">+</button>
-        <div className="counter__digit">
-          {counter}
-        </div>
-        <button onClick={() => setCounter((prev) => prev - 1)} className="counter__button counter__minus">-</button>
-      </div>
     </>
   );
 }
